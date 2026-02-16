f92914c2ce553a75a1c1c714cce162d5fad64c17
Juan Bello <141571725+Juan-Bello@users.noreply.github.com>
2026-02-15 21:57:01 -0500

    Add files via upload

--- thymeleaf/src/main/java/co/javeriana/dw/thymeleaf/Miembros/WikiController.java
+++ thymeleaf/src/main/java/co/javeriana/dw/thymeleaf/Miembros/WikiController.java
@@ -45,6 +45,12 @@ public class WikiController {
 
         return "arquitectura";
     }
+
+    /*@GetMapping("/formulario")
+    public String form(Model model) {
+
+        return "formulario";
+    }*/
     
 }
 
